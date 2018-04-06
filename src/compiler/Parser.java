8536306e5b41fd381258c35b7306b0ba89eda6b7
HongfeiJu <35699883+HongfeiJu@users.noreply.github.com>
2018-04-06 16:09:57 -0700

    add AST output

--- src/compiler/Parser.java
+++ src/compiler/Parser.java
@@ -7,6 +7,7 @@ public class Parser {
 	private static Lexcer.Token token;
 	private static Queue<Lexcer.Token> tokenList=new LinkedList<Lexcer.Token>();
 	private static Queue<String> tree=new LinkedList<String>();
+	public static AST ast=new AST("program");
 	
 	public Parser(String s) throws FileNotFoundException {
 		filename=s;
@@ -15,170 +16,202 @@ public class Parser {
 		lexcer.getTokens();
 		lexcer.printTokens();
 		tokenList=lexcer.getTokensString();
-		program();
+		program(ast);
 	}
 	
 	public Parser(Queue<Lexcer.Token> s) {
 		tokenList=s;
-		program();
+		program(ast);
 	}
 	
-	public static void program() {
+	public static void program(AST t) {
 		token=tokenList.poll();
-		tree.add("program");
-		tree.add("(");
-		statementList();
-		tree.add(")");
+		//tree.add("program");
+		//tree.add("(");
+		t.sub1=new AST("stmtList");
+		statementList(t.sub1);
+		//tree.add(")");
 	}
 	
-	public static void statementList() {
-		tree.add("stmtList");
-		tree.add("(");
-		statement();
+	public static void statementList(AST t) {
+		//tree.add("stmtList");
+		//tree.add("(");
+		t.sub1=new AST("stmt");
+		statement(t.sub1);
 		//System.out.println("debug:"+token);
 		if (token==null) return;
 		else if (token.tokenType.equals("VAR")||token.tokenType.equals("IDENTIFIER")||token.tokenType.equals("IF")
 		  ||token.tokenType.equals("WHILE")||token.tokenType.equals("PRINT")) {
-			statementList();
+			t.sub2=new AST("stmtList");
+			statementList(t.sub2);
 		}
-		tree.add(")");
+		//tree.add(")");
 	}
 	
-	public static void statement() {
-		tree.add("stmt");
-		tree.add("(");
-		if (token.tokenType.equals("VAR")) declaration();
-		else if (token.tokenType.equals("IDENTIFIER")) assignment();
-		else if (token.tokenType.equals("IF")) ifStatement();
-		else if (token.tokenType.equals("WHILE")) whileStatement();
-		else if (token.tokenType.equals("PRINT")) printFunc();
-		tree.add(")");
+	public static void statement(AST t) {
+		//tree.add("stmt");
+		//tree.add("(");
+		if (token.tokenType.equals("VAR")) {
+			t.sub1=new AST("dcl");
+			declaration(t.sub1);
+		}
+		else if (token.tokenType.equals("IDENTIFIER")) {
+			t.sub1=new AST("asgm");
+			assignment(t.sub1);
+		}
+		else if (token.tokenType.equals("IF")) {
+			t.sub1=new AST("if");
+			ifStatement(t.sub1);
+		}
+		else if (token.tokenType.equals("WHILE")) {
+			t.sub1=new AST("while");
+			whileStatement(t.sub1);
+		}
+		else if (token.tokenType.equals("PRINT")) {
+			t.sub1=new AST("print");
+			printFunc(t.sub1);
+		}		
+		//tree.add(")");
 	}
 	
-	public static void declaration() {		
-		tree.add("dcl");
-		tree.add("(");
-		match("VAR");		
-		tree.add(identifier());
+	public static void declaration(AST t) {		
+		//tree.add("dcl");
+		//tree.add("(");
+		match("VAR");
+		t.sub1=identifier();
+		//tree.add(identifier(t));
 		match("SEMICOLON");
-		tree.add(")");
+		//tree.add(")");
 	}
 	
-	public static void assignment() {
-		tree.add("asgm");
-		tree.add("(");
-		tree.add(identifier());
+	public static void assignment(AST t) {
+		//tree.add("asgm");
+		//tree.add("(");
+		//tree.add(identifier(t));
+		t.sub1=identifier();
 		match("ASSIGNMENT");
-		for(String x:lowExpression()) {
+		t.sub2=lowExpression();
+		/*for(String x:lowExpression(t)) {
 			tree.add(x);
-		};
+		};*/
 		match("SEMICOLON");
-		tree.add(")");
+		//tree.add(")");
 	}
 	
-	public static void ifStatement() {
-		tree.add("if");
-		tree.add("(");
+	public static void ifStatement(AST t) {
+		//tree.add("if");
+		//tree.add("(");
 		match("IF");
 		match("LPAREN");
-		booleanExpression();
+		t.sub1=booleanExpression();
 		match("RPAREN");
 		match("CORRECT");
 		match("LCBRACKET");
-		statementList();
+		t.sub2=new AST("stmtList");
+		statementList(t.sub2);
 		match("RCBRACKET");
 		if (token.tokenType.equals("WRONG")) {
+			
 			match("WRONG");
 			match("LCBRACKET");
-			statementList();
+			t.sub3=new AST("stmtList");
+			statementList(t.sub3);
 			match("RCBRACKET");
 		}
-		tree.add(")");
+		//tree.add(")");
 	}
 	
-	public static void whileStatement() {
-		tree.add("while");
-		tree.add("(");
+	public static void whileStatement(AST t) {
+		//tree.add("while");
+		//tree.add("(");
 		match("WHILE");
 		match("LPAREN");
-		booleanExpression();
+		t.sub1=booleanExpression();
 		match("RPAREN");
 		match("LCBRACKET");
-		statementList();
+		t.sub2=new AST("stmtList");
+		statementList(t.sub2);
 		match("RCBRACKET");
-		tree.add(")");
+		//tree.add(")");
 	}
 	
-	public static void printFunc() {
-		tree.add("print");
-		tree.add("(");
+	public static void printFunc(AST t) {
+		
+		//tree.add("print");
+		//tree.add("(");
 		match("PRINT");
-		tree.add(identifier());
+		t.sub1=identifier();
+		//tree.add(identifier(t));
 		match("SEMICOLON");
-		tree.add(")");
+		//tree.add(")");
 	}
 	
-	public static void booleanExpression() {
+	public static AST booleanExpression() {
+		AST t=new AST("");
 		if (token.tokenType.equals("TRUE")) {
-			tree.add("true");
+			//tree.add("true");
 			match("TRUE");
+			return new AST("true");
 		}else if (token.tokenType.equals("FALSE")) {
-			tree.add("false");
+			//tree.add("false");
 			match("FALSE");
+			return new AST("false");
 		}else {
-			Queue<String> templow1=lowExpression();
+			t.sub1=lowExpression();
 			switch(token.tokenType) {
 			case "EQUAL":{
-				match("EQUAL");tree.add("equal");tree.add("(");
+				match("EQUAL");
+				t.operator="EQUAL";
+				/*tree.add("equal");tree.add("(");
 				for(String x:templow1) {
 					tree.add(x);
-				};
+				};*/
 				break;
-			}
+				}
 			case "NOLARGERTHAN":{
-				match("NOLARGERTHAN");tree.add("NoLargerThan");tree.add("(");
-				for(String x:templow1) {
-					tree.add(x);
-				};
+				match("NOLARGERTHAN");
+				t.operator="NOLARGERTHAN";
 				break;
-			}
+				}
 			case "NOLESSTHAN":{
-				match("NOLESSTHAN");tree.add("NoLessThan");tree.add("(");
-				for(String x:templow1) {
-					tree.add(x);
-				};
+				match("NOLESSTHAN");
+				t.operator="NOLESSTHAN";
 				break;
-			}
+				}
 			case "NOTEQUAL":{
-				match("NOTEQUAL");tree.add("NotEqual");tree.add("(");
-				for(String x:templow1) {
-					tree.add(x);
-				};
+				match("NOTEQUAL");
+				t.operator="NOTEQUAL";
 				break;
-			}
-			case "ASSIGNMENT":{
-				match("ASSIGNMENT");tree.add("assignment");tree.add("(");
-				for(String x:templow1) {
-					tree.add(x);
-				};
+				}
+			case "LARGERTHAN":{
+				match("LARGERTHAN");
+				t.operator="LARGERTHAN";
 				break;
-			}			
+				}	
+			case "LESSTHAN":{
+				match("LESSTHAN");
+				t.operator="LESSTHAN";
+				break;
+				}
 			}
-			Queue<String> templow2=lowExpression();
-			for(String x:templow2) {
-				tree.add(x);
-			};
-			tree.add(")");
+			t.sub2=lowExpression();			
+			}
+			//tree.add(")");
+		return t;
 		}
-	}
 	
-	public static Queue<String> lowExpression() {
-		Queue<String> tempHigh=highExpression();
-		Queue<String> temp=new LinkedList<String>();
+	public static AST lowExpression() {
+		AST t=new AST("");
+		//Queue<String> tempHigh=highExpression();
+		//Queue<String> temp=new LinkedList<String>();
+		t.sub1=highExpression();
 		if (token.tokenType.equals("PLUS")||token.tokenType.equals("MINUS")) {
 			switch(token.tokenType) {
 			case "PLUS":{
-				match("PLUS");temp.add("plus");temp.add("(");
+				match("PLUS");
+				t.operator="PLUS";
+				t.sub2=lowExpression();
+				/*temp.add("plus");temp.add("(");
 				for(String x:tempHigh) {
 					temp.add(x);
 				};
@@ -186,11 +219,14 @@ public class Parser {
 				for(String x:tempLow) {
 					temp.add(x);
 				};
-				temp.add(")");
+				temp.add(")");*/
 				break;
 			}
 			case "MINUS":{
-				match("MINUS");temp.add("minus");temp.add("(");
+				match("MINUS");
+				t.operator="MINUS";
+				t.sub2=lowExpression();
+				/*temp.add("minus");temp.add("(");
 				for(String x:tempHigh) {
 					temp.add(x);
 				};
@@ -198,61 +234,66 @@ public class Parser {
 				for(String x:tempLow) {
 					temp.add(x);
 				};
-				temp.add(")");
+				temp.add(")");*/
 				break;
 				}
 			}			
 		}else {
-			for(String x:tempHigh) {
-				temp.add(x);
-			};
+			return t.sub1;
 		}		
-		return temp;
+		return t;
 	}
 	
-	public static Queue<String> highExpression() {
-		Queue<String> temp=new LinkedList<String>();
-		String s="";
+	public static AST highExpression() {
+		AST t=new AST("");
 		if (token.tokenType.equals("IDENTIFIER")) {
-			s=identifier();
+			t.sub1=identifier();
 		}else if (token.tokenType.equals("NUMBER")) {
-			s=number();	
+			t.sub1=number();	
 		}
 		
 		if (token.tokenType.equals("MULTIPLE")||token.tokenType.equals("DIVIDE")) {
 			switch(token.tokenType) {
 			case "MULTIPLE":{
-				match("MULTIPLE");temp.add("multiple");temp.add("(");temp.add(s);
+				t.operator="MULTIPLE";
+				match("MULTIPLE");
+				t.sub2=highExpression();
+				/*temp.add("multiple");temp.add("(");temp.add(s);
 				for(String x:highExpression()) {
 					temp.add(x);
 				};
-				temp.add(")");
+				temp.add(")");*/
 				break;
 			}
 			case "DIVIDE":{
-				match("DIVIDE");temp.add("divide");temp.add("(");temp.add(s);
+				t.operator="DIVIDE";
+				match("DIVIDE");
+				t.sub2=highExpression();
+				/*temp.add("divide");temp.add("(");temp.add(s);
 				for(String x:highExpression()) {
 					temp.add(x);
 				};
-				temp.add(")");break;
+				temp.add(")");break;*/
 			}
 			}			
 		}else {
-			temp.add(s);
+			return t.sub1;
 		}
-		return temp;
+		return t;
 	}
 	
-	public static String number() {		
-		String value=token.value;		
+	public static AST number() {		
+		String value=token.value;	
+		AST t=new AST(value);
 		match("NUMBER");
-		return value;
+		return t;
 	}
 	
-	public static String identifier() {
+	public static AST identifier() {
 		String value=token.value;
+		AST t=new AST(value);
 		match("IDENTIFIER");
-		return value;
+		return t;
 	}
 	
 	public static void match(String tokenType) {	
@@ -282,6 +323,37 @@ public class Parser {
 			
 		}
 	}
+	public static void printAST() {
+		printAST(ast);
+	}
+	public static void printAST(AST t) {
+		
+		if (t==null) return;
+		
+		boolean hasSub=hasSub(t);
+		System.out.print(t.operator);
+		if (hasSub) System.out.print("(");
+		if (t.sub1!=null) printAST(t.sub1);
+		if (t.sub2==null) {
+			if (hasSub) System.out.print(")");
+		}else {
+			System.out.print(", ");
+			printAST(t.sub2);
+		}
+		if (t.sub3==null) {
+			if (hasSub) System.out.print(")");
+		}else {
+			System.out.print(", ");
+			printAST(t.sub3);
+			if (hasSub) System.out.print(")");
+		}
+		
+	}
+	
+	public static boolean hasSub(AST t) {
+		return t.sub1!=null||t.sub2!=null||t.sub3!=null;
+	}
+	
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		String filename="C:/Users/Hongfei/Desktop/codes.txt";
@@ -297,8 +369,7 @@ public class Parser {
 		tokenList.add(new Lexcer.Token("SEMICOLON",";"));
 		
 		Parser parser=new Parser(filename);
-		//parser.printTree();
-		parser.printTree();
+		parser.printAST();
 		
 		
 	}
